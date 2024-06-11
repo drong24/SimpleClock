c19e50f79b65fda8e7d3ff9b08e4b6d07d86ff6b
drong24 <daisyrong24@gmail.com>
2024-06-11 15:49:20 -0700

    added clock label and contraints

--- SimpleClock/SimpleClock/ViewController.swift
+++ SimpleClock/SimpleClock/ViewController.swift
@@ -11,16 +11,34 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var dateTimeLabel: UILabel!
     
-    var currentDateTime = Date()
+    @IBOutlet weak var bgImage: UIImageView!
     
+    @IBOutlet weak var durationPicked: UIDatePicker!
+    
+    @IBOutlet weak var countdownLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let df = DateFormatter()
-        df.dateFormat = "yyyy-MM-dd hh:mm:ss"
-        let now = df.string(from: Date())
-        dateTimeLabel.text = now
-        print(currentDateTime)
+        df.dateFormat = "E, d MMM yyyy HH:mm:ss"
+        // updates date and time per second
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { Timer in
+            let now = df.string(from: Date())
+            self.dateTimeLabel.text = now
+            let hour = Calendar.current.component(.hour, from: Date())
+            // changes background image depending if its am or pm
+            if (hour < 12) {
+                self.bgImage.image = UIImage.dayBackground
+            }
+            else {
+                self.bgImage.image = UIImage.nightBackground
+            }
+        }
+    }
+    
+    @IBAction func timerButton(_ sender: UIButton) {
+        countdownLabel.text =  String(durationPicked.countDownDuration)
     }
 }
 
